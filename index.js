2189e13803f7bd1483ba56e615bbbeb4a28a197f
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-22 17:15:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ client.on("guildMemberRemove", member => {
 
 // réaction émote //
 const responseObject = {
-    `:joy:`: "il est content il rigole pour rien -_-'"
+    `😥`: "il est content il rigole pour rien -_-'"
 };
 
 client.on("message", (message) => {
