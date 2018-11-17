f89c9f127d7232ba5a1ea020fbf2eceb095856ed
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-11-17 21:20:01 +0100

    Add files via upload

---
+++ commands/fack.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+    message.delete();
+    message.channel.send("FUUUUUUUUCK https://www.youtube.com/watch?v=6aK78w-st2U").catch(console.error);
+    console.log
+}
