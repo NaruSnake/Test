91804ea220b53bf95b38dd4e4c098b6692ab2546
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-11-17 21:18:25 +0100

    Add files via upload

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,10 +1,6 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js');
-    const embed = new Discord.RichEmbed()
-    .setColor(0x00FE00)
-    .setDescription("https://www.youtube.com/watch?v=6aK78w-st2U")
     message.delete();
-    message.channel.send("FUUUUUUUUCK").catch(console.error);
-    message.channel.send({embed}).catch(console.error);
+    message.channel.send("FUUUUUUUUCK https://www.youtube.com/watch?v=6aK78w-st2U").catch(console.error);
     console.log
 }
