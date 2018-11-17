0ec877dac335d6bbf3e9372f7d72aab9cbaf2855
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-11-17 21:28:22 +0100

    Add files via upload

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,6 +1,11 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js');
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00FE00)
+    .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
+    .setURL("https://www.youtube.com/watch?v=6aK78w-st2U")
     message.delete();
-    message.channel.send("FUUUUUUUUCK https://www.youtube.com/watch?v=6aK78w-st2U").catch(console.error);
+    message.channel.send("FUUUUUUCK").catch(console.error);
+    message.channel.send({embed}).catch(console.error);
     console.log
 }
